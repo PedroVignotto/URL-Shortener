fcd74d0629ab93e15f60a51f35428b10e193bc37
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-21 18:44:38 -0300

    feat: ensure shortenURLUseCase return code on success

--- src/domain/use-cases/shorten-url.ts
+++ src/domain/use-cases/shorten-url.ts
@@ -3,10 +3,11 @@ import { CodeGenerator } from '@/domain/contracts/gateways'
 
 type Setup = (codeGenerator: CodeGenerator, urlRepository: AddURLRepository) => ShortenURL
 type Input = { originalURL: string }
-type Output = void
+type Output = string
 export type ShortenURL = (input: Input) => Promise<Output>
 
 export const shortenURLUseCase: Setup = (codeGenerator, urlRepository) => async ({ originalURL }) => {
   const code = await codeGenerator.generate()
   await urlRepository.create({ originalURL, code })
+  return code
 }
