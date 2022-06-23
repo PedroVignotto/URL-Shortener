5aa7d5c9359f89f409461c8b03f7a74fa6a49dee
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-23 16:23:55 -0300

    refactor: move getRandomCharacter logic to a separate function

--- src/infra/gateways/code.ts
+++ src/infra/gateways/code.ts
@@ -3,12 +3,14 @@ import { CodeGenerator } from '@/domain/contracts/gateways'
 export class Code implements CodeGenerator {
   constructor (private readonly maxCodeLength: number) {}
 
-  async generate (): Promise<CodeGenerator.Output> {
+  public async generate (): Promise<CodeGenerator.Output> {
     let code = ''
-    const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for (let i = 0; i < this.maxCodeLength; i++) {
-      code += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length))
-    }
+    for (let i = 0; i < this.maxCodeLength; i++) code += this.getRandomCharacter()
     return code
+  }
+
+  private getRandomCharacter (): string {
+    const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    return possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length))
   }
 }
