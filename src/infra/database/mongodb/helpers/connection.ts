0cb07bf4addd9796bb3036e7558a6d6e8455e9c3
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-23 17:39:01 -0300

    feat: ensure MongoConnection create a new connection

--- src/infra/database/mongodb/helpers/connection.ts
+++ src/infra/database/mongodb/helpers/connection.ts
@@ -1,12 +1,17 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+import { MongoClient } from 'mongodb'
+
 export class MongoConnection {
   private static instance?: MongoConnection
+  private connection?: MongoClient | null
 
   private constructor () {}
 
   static getInstance (): MongoConnection {
     if (!MongoConnection.instance) MongoConnection.instance = new MongoConnection()
-
     return MongoConnection.instance
+  }
+
+  public async connect (uri: string): Promise<void> {
+    this.connection = await new MongoClient(uri).connect()
   }
 }
