d2ec718f0c6225e51e224c8834f4fc4edac16d06
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-23 17:18:42 -0300

    test: remove globalConfig.json after tests

--- tests/infra/database/mongodb/helpers/connection.spec.ts
+++ tests/infra/database/mongodb/helpers/connection.spec.ts
@@ -1,10 +1,16 @@
 import { MongoConnection } from '@/infra/database/mongodb/helpers'
 
+import fs from 'fs'
+
 describe('MongoConnection', () => {
   let sut: MongoConnection
 
   beforeAll(() => {
     sut = MongoConnection.getInstance()
+  })
+
+  afterAll(() => {
+    fs.unlink(process.cwd() + '/globalConfig.json', () => {})
   })
 
   it('Should have only one instance', () => {
