c9a81d8e5873d0663f5b44353c163ddee9c75c61
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-23 17:39:11 -0300

    test: ensure MongoConnection create a new connection

--- tests/infra/database/mongodb/helpers/connection.spec.ts
+++ tests/infra/database/mongodb/helpers/connection.spec.ts
@@ -1,12 +1,20 @@
 import { MongoConnection } from '@/infra/database/mongodb/helpers'
 
+import { MongoClient } from 'mongodb'
+import { mocked } from 'jest-mock'
 import fs from 'fs'
+
+jest.mock('mongodb')
 
 describe('MongoConnection', () => {
   let sut: MongoConnection
 
+  const connectSpy: jest.Mock = jest.fn()
+
   beforeAll(() => {
     sut = MongoConnection.getInstance()
+
+    mocked(MongoClient).mockImplementation(jest.fn().mockImplementation(() => ({ connect: connectSpy })))
   })
 
   afterAll(() => {
@@ -15,5 +23,11 @@ describe('MongoConnection', () => {
 
   it('Should have only one instance', () => {
     expect(sut).toBe(MongoConnection.getInstance())
+  })
+
+  it('Should create a new connection', async () => {
+    await sut.connect(process.env.MONGO_URL!)
+
+    expect(connectSpy).toHaveBeenCalled()
   })
 })
