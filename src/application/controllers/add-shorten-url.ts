d7c86b316f455da9fefdc0b268c805848fbc7f2d
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-25 19:00:52 -0300

    feat: ensure AddShortenURLController call addShortenURL with correct value

--- src/application/controllers/add-shorten-url.ts
+++ src/application/controllers/add-shorten-url.ts
@@ -1,12 +1,16 @@
 import { RequiredFieldError } from '@/application/errors'
 import { badRequest, created, HttpResponse } from '@/application/helpers'
+import { AddShortenURL } from '@/domain/use-cases'
 
 type HttpRequest = { originalURL: string }
 type Model = undefined | Error
 
 export class AddShortenURLController {
+  constructor (private readonly addShortenURL: AddShortenURL) {}
+
   async handle ({ originalURL }: HttpRequest): Promise<HttpResponse<Model>> {
     if (!originalURL) return badRequest(new RequiredFieldError('originalURL'))
+    await this.addShortenURL({ originalURL })
     return created()
   }
 }
