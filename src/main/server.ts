93f1337aafa9c2c6515b82d3846436f0ff437b82
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-26 16:44:56 -0300

    refactor: move app creation to a separate file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,4 @@
 import './config/module-alias'
-
-import express from 'express'
-
-const app = express()
+import { app } from '@/main/config/app'
 
 app.listen(3000, () => console.log('Server is running at http://localhost:3000'))
