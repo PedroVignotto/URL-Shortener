4f897a2a7986856d6dac67944849fdd95a261638
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-26 17:29:44 -0300

    feat: add Controller template

--- src/application/controllers/add-shorten-url.ts
+++ src/application/controllers/add-shorten-url.ts
@@ -1,20 +1,17 @@
+import { Controller } from '@/application/controllers'
 import { RequiredFieldError } from '@/application/errors'
-import { badRequest, created, HttpResponse, serverError } from '@/application/helpers'
+import { badRequest, created, HttpResponse } from '@/application/helpers'
 import { AddShortenURL } from '@/domain/use-cases'
 
 type HttpRequest = { originalURL: string }
 type Model = { shortURL: string } | Error
 
-export class AddShortenURLController {
-  constructor (private readonly addShortenURL: AddShortenURL) {}
+export class AddShortenURLController extends Controller {
+  constructor (private readonly addShortenURL: AddShortenURL) { super() }
 
-  async handle ({ originalURL }: HttpRequest): Promise<HttpResponse<Model>> {
-    try {
-      if (!originalURL) return badRequest(new RequiredFieldError('originalURL'))
-      const code = await this.addShortenURL({ originalURL })
-      return created({ shortURL: `${process.env.APP_URL!}/${code}` })
-    } catch (error: unknown) {
-      return serverError(error)
-    }
+  async perform ({ originalURL }: HttpRequest): Promise<HttpResponse<Model>> {
+    if (!originalURL) return badRequest(new RequiredFieldError('originalURL'))
+    const code = await this.addShortenURL({ originalURL })
+    return created({ shortURL: `${process.env.APP_URL!}/${code}` })
   }
 }
