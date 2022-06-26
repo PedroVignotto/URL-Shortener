ac3aba50e65edc8eb7be5696d1956b61ef013cbe
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-26 17:42:55 -0300

    test: ensure ExpressRouter respond with 200 and correct data

--- tests/main/adapters/express-router.spec.ts
+++ tests/main/adapters/express-router.spec.ts
@@ -22,6 +22,8 @@ describe('ExpressRouterAdapter', () => {
     value = generateRandomValue()
     req = getMockReq({ body: { [key]: value } })
     res = getMockRes().res
+
+    controller.handle.mockResolvedValue({ statusCode: 200, data: { data: value } })
   })
 
   it('Should call handle with correct request', async () => {
@@ -38,5 +40,12 @@ describe('ExpressRouterAdapter', () => {
 
     expect(controller.handle).toHaveBeenCalledWith({})
     expect(controller.handle).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should respond with 200 and correct data', async () => {
+    await sut.adapt(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith({ data: value })
   })
 })
