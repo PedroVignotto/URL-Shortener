75dfaab8bb2a9c8fb64278b171ad7dac4052b50a
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-26 16:58:29 -0300

    test: ensure cors is enable

---
+++ tests/main/middlewares/cors.test.ts
@@ -0,0 +1,11 @@
+import { app } from '@/main/config/app'
+
+import request from 'supertest'
+
+describe('CORS Middleware', () => {
+  it('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => { res.send() })
+
+    await request(app).get('/test_cors').expect('Access-control-Allow-origin', '*')
+  })
+})
