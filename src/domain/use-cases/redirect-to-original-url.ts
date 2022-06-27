f348f147e5024b1b9fd1a45c0859b4ff92d82b82
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-27 15:29:25 -0300

    feat: ensure redirectToOriginalURLUseCase return originalURL on success

--- src/domain/use-cases/redirect-to-original-url.ts
+++ src/domain/use-cases/redirect-to-original-url.ts
@@ -3,10 +3,11 @@ import { FieldNotFoundError } from '@/domain/errors'
 
 type Setup = (urlRepository: LoadURLByCodeRepository) => RedirectToOriginalURL
 type Input = { code: string }
-type Output = void
+type Output = string
 export type RedirectToOriginalURL = (input: Input) => Promise<Output>
 
 export const redirectToOriginalURLUseCase: Setup = urlRepository => async ({ code }) => {
   const originalURL = await urlRepository.loadByCode({ code })
   if (!originalURL) throw new FieldNotFoundError('code')
+  return originalURL
 }
