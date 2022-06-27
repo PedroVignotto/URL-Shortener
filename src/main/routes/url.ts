0af4bf97214a6cbe5f29f1fc59338c558707e401
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-27 17:04:46 -0300

    feat: add RedirectToOriginalURLController route

--- src/main/routes/url.ts
+++ src/main/routes/url.ts
@@ -1,8 +1,9 @@
-import { expressRouterAdapter as adapt } from '@/main/adapters'
-import { makeAddShortenURLController } from '@/main/factories/application/controllers'
+import { expressRouterAdapter as adapt, expressRedirectRouterAdapter as adaptRedirect } from '@/main/adapters'
+import { makeAddShortenURLController, makeRedirectToOriginalURLController } from '@/main/factories/application/controllers'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/url', adapt(makeAddShortenURLController()))
+  router.get('/:code', adaptRedirect(makeRedirectToOriginalURLController()))
 }
