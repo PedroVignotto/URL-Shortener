5d87a32ff9bcc9b9fcf1eab10faafc311671dda8
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-27 15:57:59 -0300

    feat: ensure RedirectToOriginalURLController return 200 with original URL on success

--- src/application/controllers/redirect-to-original-url.ts
+++ src/application/controllers/redirect-to-original-url.ts
@@ -4,14 +4,14 @@ import { badRequest, HttpResponse, ok } from '@/application/helpers'
 import { RedirectToOriginalURL } from '@/domain/use-cases'
 
 type HttpRequest = { code: string }
-type Model = undefined | Error
+type Model = { originalURL: string } | Error
 
 export class RedirectToOriginalURLController extends Controller {
   constructor (private readonly redirectToOriginalURL: RedirectToOriginalURL) { super() }
 
   async perform ({ code }: HttpRequest): Promise<HttpResponse<Model>> {
     if (!code) return badRequest(new RequiredFieldError('code'))
-    await this.redirectToOriginalURL({ code })
-    return ok(undefined)
+    const originalURL = await this.redirectToOriginalURL({ code })
+    return ok({ originalURL })
   }
 }
