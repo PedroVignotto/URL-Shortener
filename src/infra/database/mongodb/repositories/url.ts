30722c767cc230ce341c3c5d0cd32c2ba9c4bafa
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-27 15:36:33 -0300

    feat: ensure URLRepository return undefined if code does not exists

--- src/infra/database/mongodb/repositories/url.ts
+++ src/infra/database/mongodb/repositories/url.ts
@@ -1,8 +1,15 @@
-import { AddURLRepository } from '@/domain/contracts/database/repositories'
+import { AddURLRepository, LoadURLByCodeRepository } from '@/domain/contracts/database/repositories'
 import { Collection } from '@/infra/database/mongodb/repositories/collection'
 
-export class URLRepository extends Collection implements AddURLRepository {
+type Repositories = AddURLRepository & LoadURLByCodeRepository
+
+export class URLRepository extends Collection implements Repositories {
   async create ({ originalURL, code }: AddURLRepository.Input): Promise<AddURLRepository.Output> {
     await this.getCollection('urls').insertOne({ originalURL, code })
+  }
+
+  async loadByCode ({ code }: LoadURLByCodeRepository.Input): Promise<LoadURLByCodeRepository.Output> {
+    await this.getCollection('urls').findOne({ code })
+    return undefined
   }
 }
