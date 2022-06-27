57379308aa0394f96652cb81266cbd73ac224347
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-27 15:27:12 -0300

    test: ensure redirectToOriginalURLUseCase throw FieldNotFound if LoadURLByCode return undefined

--- tests/domain/use-cases/redirect-to-original-url.spec.ts
+++ tests/domain/use-cases/redirect-to-original-url.spec.ts
@@ -1,17 +1,22 @@
-import { generateRandomCode } from '@/tests/mocks'
+import { generateRandomCode, generateRandomURL } from '@/tests/mocks'
 import { LoadURLByCodeRepository } from '@/domain/contracts/database/repositories'
 import { RedirectToOriginalURL, redirectToOriginalURLUseCase } from '@/domain/use-cases'
+import { FieldNotFoundError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
 describe('redirectToOriginalURLUseCase', () => {
   let sut: RedirectToOriginalURL
+  let originalURL: string
   let code: string
 
   const urlRepository = mock<LoadURLByCodeRepository>()
 
   beforeAll(() => {
+    originalURL = generateRandomURL()
     code = generateRandomCode()
+
+    urlRepository.loadByCode.mockResolvedValue(originalURL)
   })
 
   beforeEach(() => {
@@ -23,5 +28,13 @@ describe('redirectToOriginalURLUseCase', () => {
 
     expect(urlRepository.loadByCode).toHaveBeenCalledWith({ code })
     expect(urlRepository.loadByCode).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should throw FieldNotFoundError if LoadURLByCodeRepository return undefined', async () => {
+    urlRepository.loadByCode.mockResolvedValueOnce(undefined)
+
+    const promise = sut({ code })
+
+    await expect(promise).rejects.toThrow(new FieldNotFoundError('code'))
   })
 })
